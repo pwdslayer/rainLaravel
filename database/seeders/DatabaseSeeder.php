5af3da9d8dec7e0f2dc95ba234a3769c6856740e
Your Name <you@example.com>
2025-04-07 09:36:39 +0800

    Initial commit - Project Management App

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
         ]);
         User::factory()->create([
             'id' => 2,
-            'name' => 'John Smith',
-            'email' => 'john@example.com',
+            'name' => 'Carlo Panganiban',
+            'email' => 'carlo@example.com',
             'password' => bcrypt('123.321A'),
             'email_verified_at' => time()
         ]);
